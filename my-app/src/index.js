92052110cf4043f01af9bf256c99f2f98efdc479
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2022-08-16 16:22:42 +0200

    a

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { useForm, useField, splitFormProps } from "react-form";
+
+const InputField = React.forwardRef((props, ref) => {
+  const [field, fieldOptions, rest] = splitFormProps(props);
+
+  const { getInputProps } = useField(field, fieldOptions);
+
+  return (
+    <>
+      <input {...getInputProps({ ref, ...rest })} />
+    </>
+  );
+});
+
+function App() {
+  const defaultValues = React.useMemo(
+    () => ({
+      friends: [""],
+      excluded: [""]
+    }),
+    []
+  );
+  const { Form, values, pushFieldValue, removeFieldValue } = useForm({
+    defaultValues
+  });
+
+  return (
+    <Form>
+      <div>
+        Friends
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "1rem"
+          }}
+        >
+          {values.friends.map((friend, i) => (
+            <div key={i}>
+              <label>
+                Friend: <InputField field={`friends.${i}`} />{" "}
+              </label>
+              <label>
+                Excluded: <InputField field={`excluded.${i}`} />{" "}
+              </label>
+              <button
+                  type="button"
+                  onClick={() => removeFieldValue("friends", i)}
+                >
+                  X
+                </button>
+            </div>
+          ))}
+          <button type="button" onClick={() => pushFieldValue("friends", "")}>
+            Add Friend
+          </button>
+          <button type="button" onClick={() => pushFieldValue("friends", "")}>
+            Excluded Friend
+          </button>
+        </div>
+      </div>
+    </Form>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
